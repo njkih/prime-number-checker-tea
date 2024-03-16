ea00edf08b26492ecbd0f30bd40eaf36f59fae36
dfghjkcvbnmnbvcx <163094726+njkih@users.noreply.github.com>
2024-03-17 00:39:38 +0700

    init project

---
+++ index.js
@@ -0,0 +1,21 @@
+// index.js
+
+function isPrime(number) {
+    if (number <= 1) {
+      return false;
+    }
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  const number = 17;
+  if (isPrime(number)) {
+    console.log(`${number} is a prime number.`);
+  } else {
+    console.log(`${number} is not a prime number.`);
+  }
+  
